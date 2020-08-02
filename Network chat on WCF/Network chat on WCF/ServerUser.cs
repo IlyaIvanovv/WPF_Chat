14888a80747e86e1008988c362703541440176a0
HOME\uSER <ivanov_nord@mail.ru>
2020-08-02 22:03:09 +0300

    client-server WCF Chat

---
+++ Network chat on WCF/Network chat on WCF/ServerUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace Network_chat_on_WCF
+{
+	public class ServerUser
+	{
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+
+		public OperationContext operationContext { get; set; }
+
+	}
+}
